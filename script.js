e88f119bdc2436f8a66198227b7c3dbab45c57ef
Andres David Blandon <dandresavid@hotmail.com>
2022-07-22 16:26:23 -0500

    Added a flat promise chain to display countries

--- script.js
+++ script.js
@@ -81,7 +81,17 @@ getCountryAndNeighbour('colombia');
 const getCountryData = function(country){
     fetch(`https://restcountries.com/v3.1/name/${country}`)
     .then(response =>  response.json())
-    .then(data => renderCountry(data[0]));
+    .then(data => {
+        renderCountry(data[0]);
+        const neighbour = data[0].borders?.[0];
+        if(!neighbour) return;
+        
+        //Country 2
+        return fetch(`https://restcountries.com/v3.1/alpha/${neighbour}`)
+        .then( response => response.json())
+        .then( data2  => renderCountry(data2[0],'neighbour'));
+
+    });
 };
 getCountryData('portugal');
 
