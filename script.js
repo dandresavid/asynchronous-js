47611d4018aac7d8391afeafc83b3889a583733b
Andres David Blandon <dandresavid@hotmail.com>
2022-07-23 14:12:52 -0500

    Added error handling for the countries that are not found

--- script.js
+++ script.js
@@ -22,6 +22,10 @@ const renderCountry = function(data, className = '') {
     countriesContainer.insertAdjacentHTML('beforeend', html);
     countriesContainer.style.opacity = 1;
 };
+const renderError = function(msg) {
+    countriesContainer.insertAdjacentText('beforeend', msg);
+    countriesContainer.style.opacity = 1;
+}
 /*
 const getCountryAndNeighbour = function(country) {
 
@@ -75,23 +79,41 @@ getCountryAndNeighbour('colombia');
 // }, 1000);
 */
 
-
 //
 
+const getJSON = function(url, errorMsg = 'Something went wrong') {
+    return fetch(url).then(response =>  {
+        if(!response.ok)
+            throw new Error(`${errorMsg} (${response.status})`);
+        return response.json()
+    })
+}
+
 const getCountryData = function(country){
-    fetch(`https://restcountries.com/v3.1/name/${country}`)
-    .then(response =>  response.json())
+
+    getJSON(`https://restcountries.com/v3.1/name/${country}`, 'Country not found')
     .then(data => {
         renderCountry(data[0]);
         const neighbour = data[0].borders?.[0];
-        if(!neighbour) return;
+        if(!neighbour) throw new Error('No neighbour found');
         
         //Country 2
-        return fetch(`https://restcountries.com/v3.1/alpha/${neighbour}`)
-        .then( response => response.json())
-        .then( data2  => renderCountry(data2[0],'neighbour'));
-
-    });
+        return getJSON(`https://restcountries.com/v3.1/alpha/${neighbour}`, `Country not found`);
+    })  
+    .then( data2  => renderCountry(data2[0],'neighbour'))
+    .catch( err => {
+        console.error(`${err} 💥 💥 💥 💥  `);
+        renderError(`Something went wrong  💥 💥 💥 💥 ${err.message}. Try again!`);
+    })
+    .finally(()=> {
+        countriesContainer.style.opacity = 1;
+    })
 };
-getCountryData('portugal');
+
+btn.addEventListener('click', function(){
+    getCountryData('portugal');
+
+})
+
+getCountryData('australia');
 
